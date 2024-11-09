02a0befe3900a5083b74e8539340be558159492e
shsarfaraz <shsarfaraz@msn.com>
2024-11-10 01:53:12 +0500

    Final Files

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    
+      <div 
+      className="text-[75px] pl-[36px] weight-bold  text-white text-h[134 px] bg-[#A29875]">MANZZARI </div>
+
+      // "text- h-[134px] bg-[#A29875]"> MANZZARI </div>
+    
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,40 @@
+import React from "react";
+import Header from "./components/Header";
 import Image from "next/image";
 
-export default function Home() {
+function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen">
+      <Header />
+     <div className="flex">
+      <div className="w-1/2 flex flex-col justify-center">
+      <h1 className="text-[40px] weight-bold mx-44">IMPECCABLE CRAFTSMANSHIP AND FINESSE  </h1>
+       <p className="font-medium w-[902px] h-[147px] text-[32px] my-10 
+       mx-44 leading-[49.35px] tracking-[0.025em] text-[#787054] ">
+  An example of intricate workmanship and detail, elegant 
+  necklaces and long and short chains form a part of our
+   desirable collection.
+</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <button className="bg-[#A29875] h-[58px] w-[288px]
+      text-[30px] font-[500] rounded-[10px] text-white 
+      text-center flex justify-center items-center py-10  mx-44 my-10" >
+        Explore Now
+</button>
+
+      </div>
+      <div className="w-1/2 h-[] flex justify-center top-left-150
+                      items-center rounded-[1px] mt-[50px] ml-[50px]">
+      <Image 
+          src="/images/Image-2.jpg"
+          alt="Picture of the author"
+          width={490}
+          height={667}
+        />  
+      </div>  
+    </div>
     </div>
   );
-}
+}   
+
+export default Home;
